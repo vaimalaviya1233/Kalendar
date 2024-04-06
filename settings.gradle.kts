10a07892b6eff26e2a27dd38a34b2c050f0e040b
Himanshu <hello2himanshusingh@gmail.com>
2024-04-06 12:55:25 +0200

    New Build Init

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+plugins {
+    id("de.fayard.refreshVersions") version "0.60.5"
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "kalendar-new"
+include(":app")
+include(":kalendar")
